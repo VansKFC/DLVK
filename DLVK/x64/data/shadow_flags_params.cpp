5b2d3eb628c666cfd3ce6669c52291ab612430fd
VansKFC <74155424+VansKFC@users.noreply.github.com>
2020-11-15 14:54:15 -0800

    Update shadow_flags_params.cpp
    Tweaked for aesthetic and performance enhancements.

--- DLVK/x64/data/shadow_flags_params.cpp
+++ DLVK/x64/data/shadow_flags_params.cpp
@@ -1,9 +1,9 @@
 //Reduce shadow intensity for cleaner load
 sub main(){
 shadow_area_limit_0(-1.0);
-shadow_area_limit_1(0.8 * 0.8 * 3);
-shadow_area_limit_2(1.6 * 1.6 * 3);
-shadow_area_limit_3(3.2 * 3.2 * 3);
+shadow_area_limit_1(0.4 * 0.4 * 2);
+shadow_area_limit_2(1.3 * 1.6 * 2);
+shadow_area_limit_3(2.1 * 2.1 * 2);
 ray_distance(1);
 max_rays_count(5);
 mesh("slums_pipeline_a"){
