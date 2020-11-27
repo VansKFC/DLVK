b40d955067aea85a484b2f52ae7fc422f5c5b2b7
VansKFC <74155424+VansKFC@users.noreply.github.com>
2020-11-27 14:50:49 +0900

    test

---
+++ DLVK/x64/data/quickaccessvarsdefault.cpp
@@ -0,0 +1,9 @@
+sub main(){
+Page("QuickAccessVars"){
+Alias("FPS","FPS","240","min");
+Alias("OneODEUpdate", "OneODEUpdate","4.f","max");
+Alias("DPCnt","DPCnt","100","max");
+Alias("VisObjCnt","VisObjCnt",100,"max");
+Alias("PrerndObjCnt","PrerndObjCnt",100,"max");
+Alias("NumTrees","NumTrees",100,"max");
+Alias("Body count","Body count",100,"max");}}
