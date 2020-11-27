55e9ca31e48a3ad75ed926c3fa81462c20f5fde9
VansKFC <74155424+VansKFC@users.noreply.github.com>
2020-11-27 13:21:09 +0900

    Update shadow_flags_params.cpp

--- DLVK/x64/data/shadow_flags_params.cpp
+++ DLVK/x64/data/shadow_flags_params.cpp
@@ -1,30 +1,29 @@
 //Reduce shadow intensity for cleaner load
 sub main(){
 shadow_area_limit_0(-1.0);
-shadow_area_limit_1(0.4 * 0.4 * 2);
-shadow_area_limit_2(1.3 * 1.6 * 2);
-shadow_area_limit_3(2.1 * 2.1 * 2);
-ray_distance(1);
-max_rays_count(5);
+shadow_area_limit_1(0.4 * 0.4 * 1);
+/*Remove unneeded math.
+ray_distance(0);
+max_rays_count(0);*/
 mesh("slums_pipeline_a"){
-num_shadows(2,2);
+num_shadows(1,1);
 inc_shadows();}
 mesh("slums_pipeline_a_*"){alias("slums_pipeline_a");}
 mesh("slums_pipeline_stand_a") {alias("slums_pipeline_a");}
 mesh("slums_pipeline_bend_a_*"){alias("slums_pipeline_a");}
 mesh("fence_wire_a"){
-num_shadows(2,2);}
+num_shadows(1,1);}
 mesh("fence_wire_gate_?"){alias("fence_wire_a");}
 mesh("fence_wire_panel_?"){alias("fence_wire_a");}
 mesh("fence_wire_panel_?_des"){alias("fence_wire_a");}
 mesh("tracks_traction"){
-num_shadows(3,3);
+num_shadows(1,1);
 inc_shadows();}
 mesh("wall_str_*lod");
 mesh("wall_str_*"){
 inc_shadows();}
 mesh("walls_and_roofs"){
-num_shadows(2,2);
+num_shadows(1,1);
 inc_shadows();}
 mesh("kiosk_round_a"){alias("walls_and_roofs");}
 mesh("wall_cor_03_log_ot_a"){alias("walls_and_roofs");}
@@ -50,15 +49,15 @@ mesh("ts_dome_a"){ alias("walls_and_roofs");}
 mesh("ts_dome_a_metal"){ alias("walls_and_roofs");}
 mesh("ts_dome_top"){ alias("walls_and_roofs");}    
 mesh("wl_traffic_sign_?"){
-num_shadows(2,2);
+num_shadows(1,1);
 inc_shadows();}
 mesh("street_stand_b"){
-num_shadows(2,2);
+num_shadows(1,1);
 inc_shadows();}
 mesh("dlc_road_barricade"){alias("street_stand_b");}
 mesh("dlc_wl_traffic_cone_a"){alias("street_stand_b");}
 mesh("dlc_highway_barrier_?"){
-num_shadows(2,2);
+num_shadows(1,1);
 inc_shadows();}
 mesh("thuja_a"){
-num_shadows(2,-1);}}
+num_shadows(1,-1);}}
