0f9071d772d0386affb038d9e4b489749554ab1e
VansKFC <74155424+VansKFC@users.noreply.github.com>
2020-12-12 05:36:33 +0900

    Reduced render load test.

---
+++ DLVK/x64/data/vis/aibodyvis.cpp
@@ -0,0 +1,12 @@
+import "BodyVis.scr"//BodyVis.cpp
+sub AiBodyVis()
+{
+	use BodyVis();
+	BonesStructureInfo()
+	{
+		SetSpinesCount(0);
+		SetNecksCount(0);
+		SetForearmsTwistCount(0);
+		SetUpperarmsTwistCount(0);
+	}
+}
