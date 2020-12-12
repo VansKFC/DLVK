decb5483eb85656874290f42a7e78e0b23235f1f
VansKFC <74155424+VansKFC@users.noreply.github.com>
2020-12-12 14:52:56 +0900

    Create zombie_body_parts.cpp

---
+++ DLVK/x64/data/zombie_body_parts.cpp
@@ -0,0 +1,13 @@
+!Zapalki()//Modified test.
+!BodyElement(s,i,i,i)
+BodyElement("Head",110,90,4)
+BodyElement("ArmL",80,60,4)
+BodyElement("ArmR",80,60,4)
+BodyElement("ForearmL",50,35,3)
+BodyElement("ForearmR",50,35,3)
+BodyElement("HandL",30,20,1)
+BodyElement("HandR",30,20,1)
+BodyElement("ThighL",110,90,6)
+BodyElement("ThighR",110,90,6)
+BodyElement("CalfL",100,80,4)
+BodyElement("CalfR",100,80,4)
