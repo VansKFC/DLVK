451b91ae1a23f86a13a851eab487a221f74d9011
VansKFC <74155424+VansKFC@users.noreply.github.com>
2021-01-14 10:15:52 +0900

    scr

--- src/data/defaultsettings.c
+++ src/data/defaultsettings.c
@@ -1,7 +1,8 @@
+sub main(){
 !Device(s,f,f,f,f,s,f,f)
 !DefaultPCDevice(i)
 !GammaSteps(i)
 GammaSteps(0)
 Device("&DisplayDevice_Monitor&",0.0,1.0,1.0,1.0,"curves_def.dds",0,0.0)
 Device("&DisplayDevice_TV&",0.0,1.0,1.0,1.0,"curves_def.dds",0,0.0)
-DefaultPCDevice(1)
+DefaultPCDevice(1) return 0;}
