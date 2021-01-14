8d8ae34a03290e019883db883a489dd9d501964b
VansKFC <74155424+VansKFC@users.noreply.github.com>
2021-01-14 14:13:35 +0900

    Update Bite_BloodFromMouth_A_emiter.fx.c

--- src/data/scripts/effects/bite/Bite_BloodFromMouth_A_emiter.fx.c
+++ src/data/scripts/effects/bite/Bite_BloodFromMouth_A_emiter.fx.c
@@ -1,42 +1,34 @@
 EmiterDef()
 {
-	Meter()
-
-	Space("world")
-
-	GenType("continous")
-
-	GenArea("point")
-
-	LifeTime()
-	{
-		keyf(0.000000, 2.000000)
-	}
-
-	ParticleRate()
-	{
-		keyf(0.000371, 50.658669)
-		keyf(2.000000, 0.000000)
-	}
-
-	StartVelocityRand()
-	{
-		keyv(0.000000, 0.100000, 0.100000, 0.100000)
-	}
-
-	FromEmiterVelocity()
-	{
-		keyf(0.000000, 0.003000)
-	}
-
-	Direction()
-	{
-		keyv(0.000000, 0.000000, 0.000000, -1.000000)
-	}
-
+ Meter()
+ Space("world")
+ GenType("continous")
+ GenArea("point")
+ 
+ LifeTime()
+ {
+  keyf(0.1,0.1)
+ }
+ ParticleRate()
+ {
+  keyf(0.2,0.0)
+  keyf(0.1,0.3)
+ }
+ StartVelocityRand()
+ {
+  keyv(0.2,0.9,0.0,0.3)
+ }
+ FromEmiterVelocity()
+ {
+  keyf(0.0,0.1)
+ }
+ Direction()
+ {
+  keyv(0.0,0.0,0.0,-1.0)
+ }
  TurnOffDistance()
  {
-	keyf(0.000000, 49.000000)
+  keyf(0.8,0.2)
  }
 return 0;  
 }
