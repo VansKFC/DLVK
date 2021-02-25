c5ef10bdd82d73a9a5e54340528cfac66cb4ea60
VansKFC <74155424+VansKFC@users.noreply.github.com>
2021-02-24 23:12:33 -0600

    Some basic D code for a Dying Light Vulkan Installer.
    Apologies for the horrible formatting, and statement usage. I'm new to D so this is as much as I can do for the meantime. It is very much unfinished.

--- Installers/D/main.d
+++ Installers/D/main.d
@@ -1,4 +1,22 @@
 import std.stdio;
-void main(){
-    writeln("D is amazing!");
+import std.string;
+
+int main(string[] args) {
+  
+  write("---------------------------------\n");
+  write("Dying Light Vulkan Installer | D\n");
+  write("---------------------------------\n");
+  write("Install an Official or Custom build?: \n");
+  write(" \n");
+  write(" \n");
+  write("Chose : ");
+
+ auto input = strip(stdin.readln());
+ if(input == "Official") {
+  write("Install which version?\n");
+  }
+  if(input == "Custom") {
+    write("Install what Fork?\n");
+  }
+ return 0;
 }
